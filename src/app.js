31a3d14128151de2bb9563e36d370eec00a19e88
Rodolfo Porras <porrasrodolfo7@gmail.com>
2026-01-12 16:28:21 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function cartaSelect(){
+
+const numeros = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const palos = ["♦","♥","♠","♣"]
+const elemento1 = "♦";
+const elemento2 = "♥";
+
+let numerosSelect = Math.floor(Math.random() * numeros.length)
+let numeroElegido = numeros[numerosSelect]
+
+let palosSelect = Math.floor(Math.random() * palos.length)
+let paloElegido = palos[palosSelect]
+
+if (paloElegido === elemento1 || paloElegido === elemento2) {
+    palo1.style.color = 'red'
+    palo2.style.color = 'red'
+}
+document.getElementById('palo1').innerText = paloElegido
+document.getElementById('palo2').innerText = paloElegido
+document.getElementById('numero').innerText = numeroElegido 
+}
+cartaSelect();
